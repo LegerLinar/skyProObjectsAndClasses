b2c431e32a6c80ec299889b3b023cc09da21ac3c
pavelbesedin <pavelbesedin97@mail.ru>
2023-03-31 03:05:33 +0300

    Author getters

--- src/pro/sky/java/course1/objectsAndClasses1/Author.java
+++ src/pro/sky/java/course1/objectsAndClasses1/Author.java
@@ -1,9 +1,9 @@
 package pro.sky.java.course1.objectsAndClasses1;
 //Создайте класс Book, который содержит в себе данные о названии, авторе и годе публикации книги.
-// Типы полей должны быть String, Author (его мы создадим в п. 2) и int.
-//Создайте класс Author, который содержит в себе данные об имени и фамилии автора.
-//Напишите конструкторы для обоих классов, заполняющие все поля.
-//Создайте геттеры для всех полей автора и всех полей книги.
+// Типы полей должны быть String, Author (его мы создадим в п. 2) и int. +
+//Создайте класс Author, который содержит в себе данные об имени и фамилии автора. +
+//Напишите конструкторы для обоих классов, заполняющие все поля. +
+//Создайте геттеры для всех полей автора и всех полей книги. -
 //Создайте сеттер для поля «Год публикации» у книги.
 //В методе main создайте несколько объектов «Книга» (достаточно двух)
 // и несколько объектов «Автор» (достаточно тоже двух) и инициализируйте их.
@@ -15,8 +15,16 @@ public class Author {
     private String authorName;
     private String authorSurname;
 
-    public Author (String name, String authorSurname) {
+    public Author(String name, String authorSurname) {
         this.authorName = name;
         this.authorSurname = authorSurname;
+    }
+
+    public String getAuthorName() {
+        return this.authorName;
+    }
+
+    public String getAuthorSurname() {
+        return this.authorSurname;
     }
 }
