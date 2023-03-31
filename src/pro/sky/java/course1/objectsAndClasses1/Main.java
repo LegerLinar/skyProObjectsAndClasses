820c1389da5d0d515dd5495fb5fa51016f623f44
pavelbesedin <pavelbesedin97@mail.ru>
2023-03-31 03:35:57 +0300

    Initialised authors/books

--- src/pro/sky/java/course1/objectsAndClasses1/Main.java
+++ src/pro/sky/java/course1/objectsAndClasses1/Main.java
@@ -2,7 +2,20 @@ package pro.sky.java.course1.objectsAndClasses1;
 
 public class Main {
     public static void main(String[] args) {
-        // код
+        // В методе main создайте несколько объектов «Книга» (достаточно двух)
+        //// и несколько объектов «Автор» (достаточно тоже двух) и инициализируйте их.
+        //// Учтите, что авторы являются обязательными и книги не могут создаваться без авторов.
+        ////Метод main не должен находиться в классах Book и Author.
+        ////Создайте отдельный класс для запуска приложения и объявите метод main в нем.
+        ////В том же методе main измените год публикации одной из книг с помощью сеттера.
+
+        Author ostrovskiy = new Author("Николай", "Островский");
+        Author tolstoy = new Author("Лев", "Толстой");
+
+        Book asTheSteelWasTempered = new Book("Как закалялась сталь", ostrovskiy, 1934);
+        Book stormBorne = new Book("Рождённые бурей", ostrovskiy, 1937);
+        Book warPeace = new Book("Война и Мир", tolstoy, 1867);
+        Book annaKarenina = new Book("Анна Каренина", tolstoy, 1877);
 
     }
 }
